5935274ba111169508639149df9011ff59dfa7cd
nmujtaba01-debug <nmujtaba01@gmail.com>
2026-05-19 22:39:29 +0530

    Implement GIS file inspection and reporting
    Add functionality to inspect GIS files and display details.Added GIS file inspector project

---
+++ gis_file_inspector.py
@@ -0,0 +1,13 @@
+files = ['roads.shp', 'rivers.shp', 'population.csv']
+
+print("GIS File Reports")
+
+for file in files:
+ 
+ 
+ print("---------------------")
+ print(file)
+ print(file.upper())
+ print(len(file))
+ print(file[0])
+ print(file[-3:])
